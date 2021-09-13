d86f468770c400b92e9e4cb9f901a4482706907c
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-14 02:03:41 +1000

    chore: update aws host url for backend api

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagramapi-env.eba-apmqumk3.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagramapi-env.eba-apmqumk3.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
